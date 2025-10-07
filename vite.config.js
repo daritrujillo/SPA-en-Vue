716941eaf757bfd2141e9b9e5e7c677855db688e
daritrujillo <darineltrujillo25@gmail.com>
2025-10-06 22:38:14 -0600

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-  base: '/spa-vue-unidad3/' // change if your repo name differs
-})
